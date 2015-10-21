aeaf93fb9e19f54fe9b1bfcbbac3b47b51290fea
jiaxihu <jiaxihu@google.com>
2015-10-20 21:52:13 -0700

    Update ts to include tsd.d.ts only.

--- public/script/app.ts
+++ public/script/app.ts
@@ -1,7 +1,7 @@
 // $ is a global variable loaded via script tag (could also be loaded once with AMD)
 // declare keyword just lets TypeScript know it is there.
 // This is an example of statically importing a library with help of Ambient Declaration ('declare var x')
-///<reference path="../../typings/jquery/jquery.d.ts"/>
+///<reference path="../../typings/tsd.d.ts"/>
 import $ = require('jquery');
 export class App {
    start() {
